e7fcdcb57a711b18b413e8cc32fb8dae15700b18
Nonki Takahashi <nonkit@outlook.jp>
2021-04-17 21:19:29 +0900

    via wireless

--- main.ts
+++ main.ts
@@ -2,19 +2,7 @@ input.onButtonPressed(Button.A, function () {
     readCO2()
     basic.showNumber(co2)
 })
-function beforeRead () {
-    if (mode == 0) {
-        serial.redirect(
-        SerialPin.P0,
-        SerialPin.P1,
-        BaudRate.BaudRate9600
-        )
-        basic.pause(500)
-        mode = 1
-    }
-}
 function readCO2 () {
-    beforeRead()
     co2 = Math.round(COZIR.readCo2())
 }
 input.onButtonPressed(Button.AB, function () {
@@ -22,16 +10,7 @@ input.onButtonPressed(Button.AB, function () {
     basic.showNumber(hu)
 })
 function sendParam () {
-    if (mode == 1) {
-        serial.redirect(
-        SerialPin.USB_TX,
-        SerialPin.USB_RX,
-        BaudRate.BaudRate9600
-        )
-        basic.pause(500)
-        mode = 0
-    }
-    serial.writeNumbers([co2, temp, hu])
+    radio.sendString("" + ([co2, temp, hu]))
 }
 input.onButtonPressed(Button.B, function () {
     readTemp()
@@ -41,36 +20,22 @@ function readTemp () {
     temp = input.temperature()
 }
 function readHu () {
-    beforeRead()
     hu = Math.round(COZIR.readRh())
 }
-let send = 0
-let _hu = 0
-let _temp = 0
-let _co2 = 0
 let temp = 0
 let hu = 0
 let co2 = 0
-let mode = 0
-mode = 0
+radio.setGroup(1)
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
+basic.pause(500)
 basic.forever(function () {
     readCO2()
     readTemp()
     readHu()
-    if (_co2 != co2) {
-        send = 1
-    } else if (_temp != temp) {
-        send = 1
-    } else if (_hu != hu) {
-        send = 1
-    } else {
-        send = 0
-    }
-    _co2 = co2
-    _temp = temp
-    _hu = hu
-    if (send == 1) {
-        sendParam()
-    }
+    sendParam()
     basic.pause(10000)
 })
