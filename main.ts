fe7204f9ff021a30ee94d8c97c8093781b65fc01
Nonki Takahashi <nonkit@outlook.jp>
2021-05-20 20:25:20 +0900

    changed to receive read command ":"

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function readRH () {
+    rh = Math.round(COZIR.readRh())
+}
 input.onButtonPressed(Button.A, function () {
     readCO2()
     basic.showNumber(co2)
@@ -6,25 +9,30 @@ function readCO2 () {
     co2 = Math.round(COZIR.readCo2())
 }
 input.onButtonPressed(Button.AB, function () {
-    readHu()
-    basic.showNumber(hu)
+    readRH()
+    basic.showNumber(rh)
 })
 function sendParam () {
-    radio.sendString("" + co2 + "," + temp + "," + hu)
+    radio.sendString("" + co2 + "," + temp + "," + rh)
 }
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == ":") {
+        readCO2()
+        readTemp()
+        readRH()
+        sendParam()
+    }
+})
 input.onButtonPressed(Button.B, function () {
     readTemp()
     basic.showNumber(temp)
 })
 function readTemp () {
-    temp = input.temperature()
-}
-function readHu () {
-    hu = Math.round(COZIR.readRh())
+    temp = Math.round(COZIR.readTemp())
 }
 let temp = 0
-let hu = 0
 let co2 = 0
+let rh = 0
 radio.setGroup(1)
 serial.redirect(
 SerialPin.P0,
@@ -32,10 +40,3 @@ SerialPin.P1,
 BaudRate.BaudRate9600
 )
 basic.pause(500)
-basic.forever(function () {
-    readCO2()
-    readTemp()
-    readHu()
-    sendParam()
-    basic.pause(10000)
-})
